c820a35db317ed6da96a79eca8bfe95462c037aa
Angie Goza <angielg9888@gmail.com>
2018-11-27 19:12:49 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -41,7 +41,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |seasons, holidays_and_supplies|
     holidays_and_supplies.collect do |holidays, supplies|
       if supplies == "BBQ"
-        holiday_hash[:seasons][:holidays]
+        holiday_hash[:seasons][:holidays].keys
         
       end
     end
